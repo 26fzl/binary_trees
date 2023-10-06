a66031bb85a32cd44e890f9a9c860c57f3b79ee0
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-10-06 06:31:32 -0700

    tasks

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,6 +1,21 @@
 #include "binary_trees.h"
 
 /**
+ * bst_min_val - finds the smallest node from a Binary Search Tree
+ * @root: a pointer to the root node of the tree
+ * Return: a pointer to the smallest node
+ */
+bst_t *bst_min_val(bst_t *root)
+{
+        bst_t *min = root;
+
+        while (min->left)
+                min = min->left;
+
+        return (min);
+}
+
+/**
  * bst_remove - removes a node from a Binary Search Tree
  * @root: a pointer to the root node of the tree where you will remove a node
  * @value: the value to remove in the tree
@@ -9,47 +24,32 @@
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
-	bst_t *temp = NULL;
+        bst_t *temp = NULL;
 
-	if (!root)
-		return (NULL);
+        if (!root)
+                return (NULL);
 
-	if (value < root->n)
-		root->left = bst_remove(root->left, value);
-	else if (value > root->n)
-		root->right = bst_remove(root->right, value);
-	else
-	{
-		if (!root->left)
-		{
-			temp = root->right;
-			free(root);
-			return (temp);
-		}
-		else if (!root->right)
-		{
-			temp = root->left;
-			free(root);
-			return (temp);
-		}
-		temp = bst_min_val(root->right);
-		root->n = temp->n;
-		root->right = bst_remove(root->right, temp->n);
-	}
-	return (root);
-}
-
-/**
- * bst_min_val - finds the smallest node from a Binary Search Tree
- * @root: a pointer to the root node of the tree
- * Return: a pointer to the smallest node
- */
-bst_t *bst_min_val(bst_t *root)
-{
-	bst_t *min = root;
-
-	while (min->left)
-		min = min->left;
-
-	return (min);
+        if (value < root->n)
+                root->left = bst_remove(root->left, value);
+        else if (value > root->n)
+                root->right = bst_remove(root->right, value);
+        else
+        {
+                if (!root->left)
+                {
+                        temp = root->right;
+                        free(root);
+                        return (temp);
+                }
+                else if (!root->right)
+                {
+                        temp = root->left;
+                        free(root);
+                        return (temp);
+                }
+                temp = bst_min_val(root->right);
+                root->n = temp->n;
+                root->right = bst_remove(root->right, temp->n);
+        }
+        return (root);
 }
